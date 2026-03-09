5469c3762ef1ffac64399b1fecd7d46f38b7f746
Pedro Lucas <143295682+plpedreira@users.noreply.github.com>
2026-03-09 13:52:55 -0300

    feat: add query params extraction and update ticket indexing

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,5 +1,6 @@
 import { routes } from "../routes/index.js";
 import { Database } from "../database/database.js";
+import { extractQueryParams } from "../utils/extractQueryParams.js";
 
 const database = new Database()
 
@@ -9,6 +10,13 @@ export function routeHandler(request, response) {
     })
 
     if (route) {
+        const routParams = request.url.match(route.path)
+
+        const { query } =routParams.groups
+        console.log(extractQueryParams(query))
+
+        request.query = query ? extractQueryParams(query) : {}
+
         return route.controller({ request, response, database })
     }
 
